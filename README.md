f2042901b085e77f34548c32db4b7e13819a8767
burnsac5040 <burnsac@me.com>
2021-01-08 00:28:41 -0600

    modified readme, removed python lines

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-## Machine Learning Algorithms from Scratch
+Machine Learning Algorithms from Scratch
+========================================
 
-This is just a collection of standard machine learning algorithms that are coded from scratch.
+A collection of standard machine learning algorithms coded with numpy.
 
-[K Nearest Neighbors](knn-scratch.ipynb)
+- [K Nearest Neighbors](knn-scratch.ipynb)
 
-[Logistic Regression](logreg-scratch.ipynb)
+- [Logistic Regression](logreg-scratch.ipynb)
--- python/knn-scratch.py
+++ python/knn-scratch.py
@@ -4,8 +4,6 @@
 # ## K Nearest Neighbors
 # Euclidean Distance 2D = $\sqrt{\sum_{i=1}^{n} (q_{i} - p_{i})^{2}}$
 
-
-
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
@@ -13,9 +11,6 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 get_ipython().run_line_magic('matplotlib', 'inline')
 plt.style.use(['dark_background'])
-
-
-
 
 class KNN:
     def fit(self, X, y):
@@ -32,9 +27,6 @@ class KNN:
         k_labels = [self.y_train[i] for i in indices]
         return np.argmax(np.bincount(k_labels))
 
-
-
-
 iris = load_iris()
 X, y = iris.data, iris.target
 
@@ -44,23 +36,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 
 print(f'X_train.shape: {X_train.shape} // y_train.shape: {y_train.shape}\nX_test.shape: {X_test.shape} // y_test.shape: {y_test.shape}')
 
-
-
-
 X_train[:5]
-
-
-
 
 y_train
 
-
-
-
 X_train[y_train == 1][:5]
-
-
-
 
 color_l = ['#D4CF72', '#F2848F', '#5DB26D']
 
@@ -73,19 +53,13 @@ loc = np.arange(0, max(y), max(y)/len(color_l))
 cb.set_ticks(loc)
 cb.set_ticklabels(['Class 0', 'Class 1', 'Class 2'])
 
-
-
-
 clf = KNN()
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test, k=3)
 
 print(f'Accuracy: {round((np.sum(y_pred == y_test) / len(y_test))*100, 2)}%')
 
-
 # ### Accuracy for different k's
-
-
 
 accs = {}
 
@@ -93,18 +67,9 @@ for k in range(1, X.shape[0]):
     y_pred = clf.predict(X_test, k=k)
     accs[k] = round((np.sum(y_pred == y_test) / y_test.size), 2)*100
 
-
-
-
 plt.rcParams['figure.dpi'] = 300
 plt.plot(list(accs.keys()), list(accs.values()), c='blueviolet')
 plt.xlabel('k')
 plt.ylabel('Accuracy')
 plt.yticks(ticks=np.arange(20, 101, 10), 
            labels=[f'{x}%' for x in np.arange(20, 101, 10)]);
-
-
-
-
-
-
